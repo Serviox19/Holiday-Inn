07d2fca1c71d9e3308ee6cf974fb18a651a39768
Servio Mora <servio.mora19@gmail.com>
2016-06-02 13:03:39 -0400

    set up folder structure and installed dependencies

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
+var logger = require('morgan');
+var PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/views"));
+app.use("/bower_components", express.static(__dirname + "/bower_components"));
 
 
+app.use('*', function(req, res) {
+  res.sendFile(process.cwd() + "public/views/index.html");
+});
 
 
 app.listen(PORT, function(req, res) {
