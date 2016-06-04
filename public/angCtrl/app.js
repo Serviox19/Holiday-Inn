0e8a9dd68b7529b67a2dfcd862d613372e28cd72
Servio Mora <servio.mora19@gmail.com>
2016-06-04 17:29:15 -0400

    dynamically loading content

--- public/angCtrl/app.js
+++ public/angCtrl/app.js
@@ -1,1 +1,8 @@
 var app = angular.module('hotels', []);
+
+app.controller('hotelsController', ['$scope', '$http', function($scope, $http){
+  $http.get('../js/hotels.json').then(function(data) {
+    // console.log(data.data);
+    $scope.hotels = data.data;
+  })
+}]);
